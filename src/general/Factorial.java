4e2fa3ca70ff4619745791f3d3bda506549eb9d4
asrivastava2310 <ashish.srivastav.jss@gmail.com>
2020-05-12 20:44:04 +0530

    Some formatting

--- src/general/Factorial.java
+++ src/general/Factorial.java
@@ -5,29 +5,28 @@ import java.math.BigInteger;
 public class Factorial {
 
 	public static void main(String[] args) {
-		   int givenNumber = 6;
-		   System.out.println("\nUsing muliplication");
-           System.out.println("Factorial of "+givenNumber+ " is: "+factorialHavingLargeResult(givenNumber));
-           
-           System.out.println("\nUsing Recursion");
-           System.out.println("Factorial of "+givenNumber+ " is: "+factorial(givenNumber));
+		int givenNumber = 6;
+		System.out.println("\nUsing muliplication");
+		System.out.println("Factorial of " + givenNumber + " is: " + factorialHavingLargeResult(givenNumber));
+
+		System.out.println("\nUsing Recursion");
+		System.out.println("Factorial of " + givenNumber + " is: " + factorial(givenNumber));
 	}
 
-	//Using BigInteger
+	// Using BigInteger
 	private static BigInteger factorialHavingLargeResult(int n) {
-	    BigInteger fact = BigInteger.ONE;
-	    for (int i = 2; i <= n; i++)
-	        fact = fact.multiply(BigInteger.valueOf(i));
-	    return fact;
+		BigInteger fact = BigInteger.ONE;
+		for (int i = 2; i <= n; i++)
+			fact = fact.multiply(BigInteger.valueOf(i));
+		return fact;
 	}
-	
-	//Using Recursion
-	private static int factorial(int n) 
-    { 
-        if (n == 0) 
-            return 1; 
-  
-        return n * factorial(n - 1); 
-    } 
+
+	// Using Recursion
+	private static int factorial(int n) {
+		if (n == 0)
+			return 1;
+
+		return n * factorial(n - 1);
+	}
 
 }
